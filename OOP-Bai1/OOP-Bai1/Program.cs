be9190477f79c88ebb027e873b87b3f8bd838af2
Phan Van De <depv.12010121034@vtc.edu.vn>
2022-02-15 12:29:22 +0700

    OOP Bai Tap 1

---
+++ OOP-Bai1/OOP-Bai1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPBai1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
