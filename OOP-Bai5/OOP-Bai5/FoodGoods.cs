1a9ca36984bfda963392e7e80d6de9efeeead6a2
Phan Van De <depv.12010121034@vtc.edu.vn>
2022-02-25 16:50:14 +0700

    Bai Tap OPP

---
+++ OOP-Bai5/OOP-Bai5/FoodGoods.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBai5
+{
+    public class FoodGoods
+    {
+        private string itemCode;//ma hang
+        private string nameItems;//ten mat hang
+        private double money;//gia tien
+        private DateTime dateOfManufacture;//ngay san xuat
+        private DateTime expirationDate;//ngay het han
+        private DateTime timeNow = DateTime.Now;
+
+        public FoodGoods()
+        {
+        }
+
+        public FoodGoods(string itemCode, string nameItems, double money, DateTime dateOfManufacture, DateTime exprirationDate)
+        {
+            if (itemCode.Equals("")) throw new Exception("Fail Required!");
+            else
+            {
+                this.itemCode = itemCode;
+                setNameItems(nameItems);
+                setMoney(money);
+                setDateOfManufacture(dateOfManufacture);
+                setExpirationDate(exprirationDate);
+            }
+        }
+
+        //setter
+        public void setItemCode(string itemCode)
+        {
+            this.itemCode = itemCode;
+        }
+
+        public void setNameItems(string nameItems)
+        {
+            this.nameItems = nameItems;
+        }
+
+        public void setMoney(double money)
+        {
+            this.money = money;
+        }
+
+        public void setDateOfManufacture(DateTime dateOfManufacture)
+        {
+            int result = DateTime.Compare(this.dateOfManufacture, DateTime.Now);
+            if (result < 0)
+                this.dateOfManufacture = dateOfManufacture;
+            else this.dateOfManufacture = dateOfManufacture;
+        }
+
+        public void setExpirationDate(DateTime expirationDate)
+        {
+            int result = DateTime.Compare(this.expirationDate, this.expirationDate);
+            if (result < 0)
+                this.expirationDate = expirationDate;
+            else this.expirationDate = expirationDate;
+        }
+
+        
+
+        //getter
+        public string getItemCode()
+        {
+            return itemCode;
+        }
+
+        public string getNameItems()
+        {
+            return nameItems;
+        }
+
+        public double getMoney()
+        {
+            return money;
+        }
+
+        public DateTime getDateOfManufacture()
+        {
+            return this.dateOfManufacture;
+        }
+
+        public DateTime getExpirationDate()
+        {
+            return this.expirationDate;
+        }
+
+        //kiem tra han su dung
+        public string check()
+        {
+            string temp;
+            int compare = DateTime.Compare(timeNow, expirationDate);
+
+            if (compare < 0)
+            {
+                temp = "Unexpired";
+            }
+            else
+            {
+                temp = "Expired";
+            }
+            return temp;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return $"{this.itemCode} {this.nameItems} {this.money, 0:0.00 VND} {dateOfManufacture} {expirationDate} {check()}";
+        }
+
+        // in bang thong tin
+        public void outPut()
+        {
+            String st = String.Format("{0}", centeredString($"{itemCode}", 20))
+          + String.Format("{0}", centeredString($"{nameItems}", 20))
+          + String.Format("{0}", centeredString($"{money, 0:0.00 VND}", 20))
+          + String.Format("{0}", centeredString($"{dateOfManufacture}", 30))
+          + String.Format("{0}", centeredString($"{expirationDate}", 30))
+          + String.Format("{0}", centeredString($"{check()}", 20));
+
+            Console.WriteLine(st);
+        }
+
+        static string centeredString(string s, int width)
+        {
+            if (s.Length >= width)
+            {
+                return s;
+            }
+
+            int leftPadding = (width - s.Length) / 2;
+            int rightPadding = width - s.Length - leftPadding;
+
+            return new string(' ', leftPadding) + s + new string(' ', rightPadding);
+        }
+    }
+}
---
+++ OOP-Bai5/OOP-Bai5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace OOPBai5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime riceDateOfManufacture = new DateTime(2003, 1, 1);
+            DateTime riceExpirationDate = new DateTime(2023, 1, 1);
+
+            FoodGoods rice = new FoodGoods();
+            rice.setItemCode("001");
+            rice.setNameItems("Rice");
+            rice.setMoney(100000);
+            rice.setDateOfManufacture(riceDateOfManufacture);
+            rice.setExpirationDate(riceExpirationDate);
+            rice.check();
+
+            DateTime noodleDateOfManufacture = new DateTime(2012, 1, 1);
+            DateTime noodleExpirationDate = new DateTime(2023, 4, 6);
+
+            FoodGoods noodle = new FoodGoods();
+            noodle.setItemCode("002");
+            noodle.setNameItems("Noodle");
+            noodle.setMoney(5000);
+            noodle.setDateOfManufacture(riceDateOfManufacture);
+            noodle.setExpirationDate(riceExpirationDate);
+            noodle.check();
+
+            DateTime waterDateOfManufacture = new DateTime(2012, 1, 1);
+            DateTime waterExpirationDate = new DateTime(2023, 4, 6);
+
+            FoodGoods water = new FoodGoods();
+            water.setItemCode("003");
+            water.setNameItems("Water");
+            water.setMoney(10000);
+            water.setDateOfManufacture(riceDateOfManufacture);
+            water.setExpirationDate(riceExpirationDate);
+            water.check();
+
+            menu();
+            Console.WriteLine();
+            rice.outPut();
+            noodle.outPut();
+            water.outPut();
+        }
+
+        // in bang thong tin
+        static void menu()
+        {
+            String st = String.Format("{0}", centeredString(" Item Code ", 20))
+              + String.Format("{0}", centeredString(" Name ", 20))
+              + String.Format("{0}", centeredString(" Money ", 20))
+              + String.Format("{0}", centeredString(" Date Of Manufacture ", 30))
+              + String.Format("{0}", centeredString(" Expiration Date ", 30))
+              + String.Format("{0}", centeredString(" Note ", 20));
+
+            Console.WriteLine(st);
+        }
+
+        static string centeredString(string s, int width)
+        {
+            if (s.Length >= width)
+            {
+                return s;
+            }
+
+            int leftPadding = (width - s.Length) / 2;
+            int rightPadding = width - s.Length - leftPadding;
+
+            return new string(' ', leftPadding) + s + new string(' ', rightPadding);
+        }
+    }
+}
